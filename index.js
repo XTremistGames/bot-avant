ca197c2b618b0445d57f4e67d76883b48b4ea1a7
XTremistGames <45567338+XTremistGames@users.noreply.github.com>
2019-01-07 18:23:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ if (message.content.startsWith("/cpower")) {
         if (!dev.has(message.author.id)) return message.reply(`\`У вас нет прав к управлению над ботом.\``) && message.delete()
         const args = message.content.slice(`/cpower`).split(/ +/);
         let pow = args.slice(1).join(" ");
-        if(pow != "1" || pow != "2") return message.reply(`\`/cpower [1 - режим модератора || 2 - режим администратора]\``) && message.delete()
+        //if(pow != "1" || pow != "2") return message.reply(`\`/cpower [1 - режим модератора || 2 - режим администратора]\``) && message.delete()
         if(pow == "1") {
         bot.destroy().then(() => {
             message.reply(`\`бот переводится в режим модератора, ожидайте\``) && message.delete()
